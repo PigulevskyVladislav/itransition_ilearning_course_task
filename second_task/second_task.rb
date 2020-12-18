b8b85673248f2876f78acc777ae586aabfdb927d
Goorly <vlad.pigulevsky@gmail.com>
2020-12-18 12:46:24 +0300

    create FourthTAsk prj

--- second_task/second_task.rb
+++
@@ -1,5 +0,0 @@
-require 'sha3'
-names = Dir['*']
-names.each do |n|
-	puts "#{n} #{SHA3::Digest.hexdigest(:sha256, File.read(Dir.getwd + '/' + n))}"
-end
--- third_task/src/by/itransition/ilearning/third_task/ThirdTask.java
+++
@@ -1,7 +0,0 @@
-package by.itransition.ilearning.third_task;
-
-public class ThirdTask {
-    public static void main(String[] args) {
-        new Dialog().BeginDialog(args);
-    }
-}
