e972c513dd75a5dc6c9a3ce044c02108de315a15
Goorly <vlad.pigulevsky@gmail.com>
2020-12-21 15:37:14 +0300

    add block/unblock logic FourthTask

--- fourth_task/FourthTask/FourthTask/PersonList.aspx.cs
+++ fourth_task/FourthTask/FourthTask/PersonList.aspx.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.ModelBinding;
+using System.Data.Entity;
 
 namespace FourthTask
 {
@@ -13,7 +14,7 @@ namespace FourthTask
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         public IQueryable<Person> GetPersons()
@@ -34,6 +35,53 @@ namespace FourthTask
                     CheckBox CheckRow = (row.Cells[0].FindControl("SelectCheckbox") as CheckBox);
                     CheckRow.Checked = CheckColumn.Checked;
                 }
+            }
+        }
+
+        protected void BlockUser_Click(object sender, EventArgs e)
+        {
+            ChangeStatus(true);
+        }
+
+        protected void UnblockUser_Click(object sender, EventArgs e)
+        {
+            ChangeStatus(false);
+        }
+
+        protected void DeleteUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChangeStatus(bool Blocked)
+        {
+            try
+            {
+                var _db = new PersonContext();
+                foreach (GridViewRow row in PersonGridView.Rows)
+                {
+                    bool selected = ((CheckBox)row.FindControl("SelectCheckbox")).Checked;
+                    if (selected)
+                    {
+                        ChangeStatusRow(row, Blocked, _db);
+                    }
+                }
+            } catch
+            {
+
+            }
+        }
+
+        private void ChangeStatusRow(GridViewRow row, bool Blocked, PersonContext _db)
+        {
+            CheckBox checkBox = (CheckBox)row.FindControl("BlockedCheckbox");
+            if (checkBox.Checked != Blocked)
+            {
+                int id = Convert.ToInt32(((Label)row.FindControl("PersonIDLabel")).Text);
+                Person person = (from p in _db.Persons where p.PersonID == id select p).FirstOrDefault();
+                person.Blocked = Blocked;
+                _db.SaveChanges();
+                checkBox.Checked = Blocked;
             }
         }
     }
--- fourth_task/FourthTask/FourthTask/PersonList.aspx.designer.cs
+++ fourth_task/FourthTask/FourthTask/PersonList.aspx.designer.cs
@@ -13,6 +13,33 @@ namespace FourthTask {
     public partial class PersonList {
         
         /// <summary>
+        /// BlockUser control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BlockUser;
+        
+        /// <summary>
+        /// UnblockUser control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button UnblockUser;
+        
+        /// <summary>
+        /// DeleteUser control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button DeleteUser;
+        
+        /// <summary>
         /// PersonGridView control.
         /// </summary>
         /// <remarks>
