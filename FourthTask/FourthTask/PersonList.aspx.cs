a02f40a6400a011d6b028ec309a36eea9c7d1952
Goorly <vlad.pigulevsky@gmail.com>
2020-12-21 14:11:47 +0300

    add select row logic FourthTask

--- FourthTask/FourthTask/PersonList.aspx.cs
+++ FourthTask/FourthTask/PersonList.aspx.cs
@@ -13,7 +13,7 @@ namespace FourthTask
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         public IQueryable<Person> GetPersons()
@@ -21,6 +21,20 @@ namespace FourthTask
             var _db = new PersonContext();
             IQueryable<Person> query = _db.Persons;
             return query;
+        }
+
+        protected void ChckedChanged(object sender, EventArgs e)
+        {
+            CheckBox CheckColumn = (PersonGridView.HeaderRow.Cells[0].FindControl("SelectCheckboxAll") as CheckBox);
+
+            foreach (GridViewRow row in PersonGridView.Rows)
+            {
+                if (row.RowType == DataControlRowType.DataRow)
+                {
+                    CheckBox CheckRow = (row.Cells[0].FindControl("SelectCheckbox") as CheckBox);
+                    CheckRow.Checked = CheckColumn.Checked;
+                }
+            }
         }
     }
 }
