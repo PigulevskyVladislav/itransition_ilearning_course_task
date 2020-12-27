aad048a7dbcbe742d5d0ad03aac2a4f4e650088e
Goorly <vlad.pigulevsky@gmail.com>
2020-12-27 05:16:03 +0300

    refactor to mssql FourthTask

---
+++ fourth_task/FourthTask/FourthTask/Models/PersonContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace FourthTask.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext() : base("FourthTask")
+        {
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
